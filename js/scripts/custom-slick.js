2cf95301ea238f806ee1a91a82b907b4b4aeb42d
MuraliJbk <murali@jbkinfotech.com>
2024-07-05 13:35:03 +0530

    test

--- js/scripts/custom-slick.js
+++ js/scripts/custom-slick.js
@@ -4,7 +4,9 @@ jQuery(document).on("ready", function(){
         postSlider.each(function(){
             const $this = jQuery(this);
             const postSlide = $this.children(".post-array-list").length;
-            if(jQuery(window).width() <= 1024){
+            const postBtn = jQuery(".post-array-btn");
+            const postHead = jQuery(".post-array-head");
+            if(jQuery(window).width() <= 1023){
                 if (!$this.hasClass('slick-initialized')) {
                     $this.slick({
                         slidesToShow: 1,
@@ -12,24 +14,36 @@ jQuery(document).on("ready", function(){
                         arrows: true,
                         prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
                         nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-                        dots: true,
-                        speed: 1500,
+                        dots: false,
+                        speed: 1000,
                         infinite: false,
                         autoplay: false,
                         variableWidth: true,
+                        appendArrows: jQuery(postHead),
+                        responsive: [
+                            {
+                                breakpoint: 743,
+                                settings: {
+                                    adaptiveHeight: true,
+                                    appendArrows: $this,
+                                    dots: true,
+                              }
+                            },
+                        ], 
                     });
                 }
             }
             else {
                 if (postSlide >= 5 && !$this.hasClass('slick-initialized')) {
                     $this.slick({
+                        appendArrows: postBtn,
                         slidesToShow: 1,
                         slidesToScroll: 1,
                         arrows: true,
                         prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
                         nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
                         dots: false,
-                        speed: 1500,
+                        speed: 1000,
                         infinite: false,
                         autoplay: false,
                         variableWidth: true,
@@ -41,7 +55,7 @@ jQuery(document).on("ready", function(){
     function destroySlider() {
         postSlider.each(function(){
             const $this = jQuery(this);
-            if(jQuery(window).width() >= 1024 && $this.hasClass('slick-initialized')) {
+            if(jQuery(window).width() >= 1023 && $this.hasClass('slick-initialized')) {
                 $this.slick('unslick');
             }
         });
