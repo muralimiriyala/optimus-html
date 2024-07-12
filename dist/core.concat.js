ddfba038b2fc799af14947ca7c3eaa51bb64ad54
MuraliJbk <murali@jbkinfotech.com>
2024-07-12 17:23:27 +0530

    test

--- dist/core.concat.js
+++ dist/core.concat.js
@@ -4349,22 +4349,6 @@ jQuery(document).ready(function($){
         $(this).parent().siblings().find('.accordion-content').slideUp(500);
     });
 
-    $(".faqs-category-btn").on('click', function(e){
-        e.preventDefault();
-        $(this).toggleClass("active");
-        $("ul.faqs-links").slideToggle(900)
-    });
-
-    const faqspan = $(".faqs-category-btn span");
-    if(window.matchMedia('(max-width: 767px)').matches){
-        $("ul.faqs-links li a").on("click", function(e){
-            e.preventDefault();
-            let text = $(this).text();
-            faqspan.html(text)
-            $(".faqs-category-btn").removeClass("active");
-            $("ul.faqs-links").slideUp(900);
-        });
-    }
 });
 
 function stickyResize() {
--- js/scripts/myscript.js
+++ js/scripts/myscript.js
@@ -9,20 +9,4 @@ jQuery(document).ready(function($){
         $(this).parent().siblings().find('.accordion-content').slideUp(500);
     });
 
-    $(".faqs-category-btn").on('click', function(e){
-        e.preventDefault();
-        $(this).toggleClass("active");
-        $("ul.faqs-links").slideToggle(900)
-    });
-
-    const faqspan = $(".faqs-category-btn span");
-    if(window.matchMedia('(max-width: 767px)').matches){
-        $("ul.faqs-links li a").on("click", function(e){
-            e.preventDefault();
-            let text = $(this).text();
-            faqspan.html(text)
-            $(".faqs-category-btn").removeClass("active");
-            $("ul.faqs-links").slideUp(900);
-        });
-    }
 });
