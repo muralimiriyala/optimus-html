7c2c64a54b6612fb02ad3aec09cf060e1c57611a
MuraliJbk <murali@jbkinfotech.com>
2024-07-22 13:16:57 +0530

    06-06-2024

--- dist/core.concat.js
+++ dist/core.concat.js
@@ -4347,49 +4347,49 @@ jQuery(document).on("ready", function(){
     initializeSlider();
 });
 
-
-jQuery(document).ready(function($){
-  $('.popup-youtube').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-video',
+jQuery(document).ready(function ($) {
+  $(".popup-youtube").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-video",
     removalDelay: 160,
     preloader: false,
     fixedContentPos: true,
     iframe: {
       patterns: {
         youtube: {
-          index: 'youtube.com/',
-          id: 'v=',
-          src: getYouTubeSrc() // Call a function to generate the appropriate YouTube URL
-        }
-      }
-    }
-  });  
+          index: "youtube.com/",
+          id: "v=",
+          src: getYouTubeSrc(), // Call a function to generate the appropriate YouTube URL
+        },
+      },
+    },
+  });
   function getYouTubeSrc() {
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    var baseSrc = 'https://www.youtube.com/embed/%id%?autoplay=1&rel=0';
+    var isChrome =
+      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    var baseSrc = "https://www.youtube.com/embed/%id%?autoplay=1&rel=0";
     if (isChrome) {
-      return baseSrc + '&mute=1';
+      return baseSrc + "&mute=1";
     } else {
       return baseSrc;
     }
   }
-  
-  $('.popup-video').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-video',
+
+  $(".popup-video").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-video",
     removalDelay: 160,
     preloader: false,
-    fixedContentPos: true
+    fixedContentPos: true,
   });
-  $('.popup-modal').magnificPopup({
-		type: 'inline',
-		fixedContentPos: true,
+  $(".popup-modal").magnificPopup({
+    type: "inline",
+    fixedContentPos: true,
     fixedBgPos: true,
-		overflowY: 'auto',
-		preloader: false,
-		removalDelay: 160,
-		mainClass: 'my-mfp-slide-top'
+    overflowY: "auto",
+    preloader: false,
+    removalDelay: 160,
+    mainClass: "my-mfp-slide-top",
   });
 });
 
