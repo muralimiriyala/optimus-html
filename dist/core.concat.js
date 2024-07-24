305b7d7c317a963216e95d8f1c76d3c1fa4b75ed
MuraliJbk <murali@jbkinfotech.com>
2024-07-24 13:05:04 +0530

    test

--- dist/core.concat.js
+++ dist/core.concat.js
@@ -4424,23 +4424,23 @@ function srchResize(){
 jQuery(document).on("ready", function(){srchResize()});
 jQuery(document).on("load", function(){srchResize()});
 jQuery(document).ready(function ($) {
-  let sbrforSlide = $(".sbr-slide-image");
-  sbrforSlide.on("click", function (e) {
+  let sbrsplideImage = $(".sbr-slide-image");
+  sbrsplideImage.on("click", function (e) {
     e.preventDefault();
     let $this = $(this);
-    let slideno = $this.data("sbr-img");
-    sbrforSlide.removeClass("sbr-active sbr-hover-active");
-    $(`.sbr-slide-image[data-sbr-img='${slideno}']`).addClass( "sbr-active" );
+    let slideno = $this.parent().data("sbr-slide");
+    $this.parent().siblings().removeClass("sbr-active sbr-hover-active");
+    $(`.sbr-for-slide[data-sbr-slide='${slideno}']`).addClass( "sbr-active" );
     $(`.sbr-slide-desktop`).hide();
     $(`.sbr-slide-desktop[data-sbr-text='${slideno}']`).fadeIn(1000);
 
-    $(`.sbr-slide-mobile`).css({ "height": "0px", })
+    $(`.sbr-slide-mobile`).css({ "max-height": "0px", })
     let $targetMobile = $(`.sbr-slide-mobile[data-sbr-text='${slideno}']`);
     let scrollHeight = $targetMobile[0].scrollHeight;
-    $targetMobile.css({ "height": `${scrollHeight}px` });
+    $targetMobile.css({ "max-height": `${scrollHeight}px` });
 
   });
-  sbrforSlide.hover(function(){
+  sbrsplideImage.hover(function(){
     let $sib = $(this).siblings();
     $($sib).children(".sbr-slide-image").filter('.sbr-active').toggleClass("sbr-hover-active")
   });
