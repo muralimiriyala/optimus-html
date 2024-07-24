8d56af7fc0bfe39018bb42dd0a8fef08df199ba1
MuraliJbk <murali@jbkinfotech.com>
2024-07-24 10:44:22 +0530

    test

--- dist/core.concat.js
+++ dist/core.concat.js
@@ -4399,19 +4399,20 @@ jQuery(document).on("ready", function(){srchResize()});
 jQuery(document).on("load", function(){srchResize()});
 jQuery(document).ready(function ($) {
   let $sbSlideImage = $(".sbr-slide-image");
-  let sbrtext  =  $(".sbr-slide-text");
-  console.log(sbrtext)
   $sbSlideImage.on("click", function (e) {
     e.preventDefault();
     let $this = $(this);
     let slideno = $this.data("sbr-img");
-    console.log(slideno)
     $sbSlideImage.removeClass("sbr-active sbr-hover-active");
     $(`.sbr-slide-image[data-sbr-img='${slideno}']`).addClass( "sbr-active" );
-    $(`.sbr-slide-text`).hide();
-    $(`.sbr-slide-desktop[data-sbr-text='${slideno}']`).fadeIn(800);
-    sbrtext
-    .style.maxHeight = `${sbrtext.scrollHeight}px`;
+    $(`.sbr-slide-desktop`).hide();
+    $(`.sbr-slide-desktop[data-sbr-text='${slideno}']`).fadeIn(1000);
+
+    $(`.sbr-slide-mobile`).css({ "height": "0px", })
+    let $targetMobile = $(`.sbr-slide-mobile[data-sbr-text='${slideno}']`);
+    let scrollHeight = $targetMobile[0].scrollHeight;
+    $targetMobile.css({ "height": `${scrollHeight}px` });
+
   });
   $sbSlideImage.hover(function(){
     let $sib = $(this).siblings();
