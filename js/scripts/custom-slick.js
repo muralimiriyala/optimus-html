5d9fae9e2ff5fca6f2dda42d195ca4e0a8fb829c
MuraliJbk <murali@jbkinfotech.com>
2024-07-25 16:49:33 +0530

    test

--- js/scripts/custom-slick.js
+++ js/scripts/custom-slick.js
@@ -1,77 +1,42 @@
 jQuery(document).ready(function ($) {
-  const postSlider = jQuery(".post-array-row");
-  function initializeSlider() {
-    postSlider.each(function () {
-      const $this = $(this);
-      const postSlide = $this.children(".post-array-list").length;
-      const postBtn = $(".post-array-btn");
-      const postHead = $(".post-array-head");
-      const postAppend = $(".post-array-appends");
-      if (window.matchMedia("(max-width: 1439px)").matches) {
-        if (!$this.hasClass("slick-initialized")) {
-          $this.slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: true,
-            prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            dots: true,
-            speed: 1000,
-            infinite: false,
-            autoplay: false,
-            variableWidth: true,
-            appendArrows: postBtn,
-            responsive: [
-              {
-                breakpoint: 1023,
-                settings: {
-                  appendArrows: postHead,
-                },
-              },
-              {
-                breakpoint: 743,
-                settings: {
-                  adaptiveHeight: true,
-                  appendArrows: postAppend,
-                  appendDots: postAppend,
-                },
-              },
-            ],
-          });
-        }
-      } else {
-        if (postSlide >= 5 && !$this.hasClass("slick-initialized")) {
-          $this.slick({
-            appendArrows: postBtn,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: true,
-            prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            dots: true,
-            speed: 1000,
-            infinite: false,
-            autoplay: false,
-            variableWidth: true,
-          });
-        }
-      }
+    const postSlider = jQuery(".post-array-slider");
+    const postBtn = $(".post-array-btn");
+    const postHead = $(".post-array-head");
+    const postAppend = $(".post-array-appends");
+    postSlider.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+      nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+      dots: true,
+      speed: 1000,
+      infinite: false,
+      autoplay: false,
+      variableWidth: true,
+      appendArrows: postBtn,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            appendArrows: postHead,
+          },
+        },
+        {
+          breakpoint: 743,
+          settings: {
+            adaptiveHeight: true,
+            appendArrows: postAppend,
+            appendDots: postAppend,
+          },
+        },
+      ],
     });
-  }
-  function destroySlider() {
-    postSlider.each(function () {
-      const $this = jQuery(this);
-      if (
-        jQuery(window).width() >= 1440 &&
-        $this.hasClass("slick-initialized")
-      ) {
-        $this.slick("unslick");
-      }
-    });
-  }
-  // Initial call
-  initializeSlider();
-  $(window).on("resize", function () { destroySlider(); initializeSlider(); });
+
+
+  
+
+
 
   function recentSlider() {
     if ($(window).width() <= 743) {
