3dc6a581ff11065c3666e87c08f16be8b8dae5b9
MuraliJbk <murali@jbkinfotech.com>
2024-07-26 16:40:55 +0530

    test

--- js/scripts/custom-stickme.js
+++ js/scripts/custom-stickme.js
@@ -1,9 +1,22 @@
-$(".aside").stickme({
-  onStick: function (e, target) {
-    // do something
-  },
-
-  onUnstick: function (e, target) {
-    // do something
-  },
+jQuery(document).ready(function ($) {
+  const $altStickme = $(".alt-stickme");
+  if ($altStickme) {
+    $altStickme.stickme({
+      onStick: function (e, target) {},
+      onUnstick: function (e, target) {},
+    });
+  }
+  $altStickme.each(function (index) {
+    let $index = index;
+    let $this = $(this).parent();
+    let nextParent = $(this).parent().closest("section");
+    nextParent.nextAll().css({
+      zIndex: $index * 2,
+      position: "relative",
+    });
+    $this.css({
+      zIndex: $index,
+      position: "relative",
+    });
+  });
 });
