a7117e84fddc00f97a5b9871e33dd3096ad8a421
MuraliJbk <murali@jbkinfotech.com>
2024-07-30 12:17:45 +0530

    test

--- dist/core.concat.js
+++ dist/core.concat.js
@@ -6379,6 +6379,16 @@ jQuery(document).ready(function ($) {
 
 
 
+document.addEventListener("DOMContentLoaded", function(){
+    const srcBtn = document.querySelector("button.res-srch-icon");
+    if(srcBtn){
+        srcBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(".res-srch-form").classList.toggle("open");
+        });
+    }
+});
+    
 jQuery(document).ready(function($) {
   let sbrsplideImage = $(".sbr-slide-image");
   sbrsplideImage.on("click", function (e) {
